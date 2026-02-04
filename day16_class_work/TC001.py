da61cdc181f9fc18f9ec738c934851faae59f9ab
Pavani <kallapavani023@gmail.com>
2026-02-04 20:08:39 +0530

    Day-16 files are added

---
+++ day16_class_work/TC001.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Chrome()
+driver.get("https://tutorialsninja.com/demo/")
+driver.maximize_window()
+driver.get("https://www.google.com/")
+print("title is", driver.title)
+time.sleep(5)
+driver.back()
+print("title after back:", driver.title)
+driver.forward()
+print("title after forward:", driver.title)
